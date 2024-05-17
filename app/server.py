84bfd6fca68786f54d3aab1166283ba1266aba42
Amol Mane <amol1994mane@gmail.com>
2024-05-16 19:04:28 -0700

    add code to load 10k based on user input

--- app/server.py
+++ app/server.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import html2text
+from sec_api import QueryApi, RenderApi
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
-from fastapi.middleware.cors import CORSMiddleware
+from langchain_core.documents.base import Document
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-import os
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 LANGCHAIN_API_KEY = os.environ.get("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 SEC_API_KEY = os.environ.get("SEC_API_KEY")
 
-print(OPENAI_API_KEY)
-print(LANGCHAIN_API_KEY)
-print(SEC_API_KEY)
-
-import html2text
-from sec_api import QueryApi, RenderApi
-from langchain import hub
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.documents.base import Document
-from langchain_openai import ChatOpenAI
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel
-
 
 app = FastAPI(
-    title="LangChain Server",
+    title="Querying 10k's using RAG",
     version="1.0",
-    description="A simple api server using Langchain's Runnable interfaces",
+    description="A simple API to retrieve and store a company's 10k into a vector database, and then query it.",
 )
 
 # Set all CORS enabled origins
@@ -44,7 +36,6 @@ app.add_middleware(
 
 
 def create_vectorstore_for_company(stock_symbol):
-    print("creating new")
     queryApi = QueryApi(api_key=SEC_API_KEY)
     query = {
         "query": "ticker:{} AND formType:\"10-K\"".format(stock_symbol),
@@ -53,72 +44,60 @@ def create_vectorstore_for_company(stock_symbol):
         "sort": [{ "filedAt": { "order": "desc" } }]
     }
     response = queryApi.get_filings(query)
-
     link_to_10k = response["filings"][0]["linkToFilingDetails"]
-    print(link_to_10k)
-
     renderApi = RenderApi(api_key=SEC_API_KEY)
-
     target_10k = renderApi.get_filing(link_to_10k)
     target_10k_full_text = html2text.html2text(target_10k)
-    print(len(target_10k_full_text))
     docs = [Document(page_content=target_10k_full_text)]
-
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=200, add_start_index=True
     )
     all_splits = text_splitter.split_documents(docs)
     vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY), persist_directory="./chroma_{}".format(stock_symbol))
-    print("finished create_vectorstore_for_company")
     return vectorstore
 
 
 def get_retriever(stock_symbol):
-    stock_symbol = stock_symbol.upper()
     vectorstore_filename = "chroma_{}".format(stock_symbol)
     if vectorstore_filename in os.listdir("./"):
         vectorstore = Chroma(persist_directory=vectorstore_filename, embedding_function=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY))
     else:
         vectorstore = create_vectorstore_for_company(stock_symbol)
-    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 2})
-    print("finished get_retriever")
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
     return retriever
 
-# vectorstore = Chroma(persist_directory="chroma_MSFT", embedding_function=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY))
-# retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-retriever = get_retriever("MSFT")
 
 template = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
 Question: {question} 
 Context: {context} 
 Answer:
 """
-
 prompt = ChatPromptTemplate.from_template(template)
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-retriever_chain = RunnableParallel({"context": retriever | format_docs, "question": RunnablePassthrough()})
-rag_chain = (
-    retriever_chain
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+retriever = None
 
 
-# @app.post("/load_10k")
-# def read_item(stock_symbol: str):
-#     _ = get_retriever(stock_symbol)
-#     return "Started loading 10k"
+@app.post("/load_10k")
+def read_item(stock_symbol: str):
+    stock_symbol = stock_symbol.upper()
+    global retriever
+    retriever = get_retriever(stock_symbol)
+    return "Loaded 10k for {}".format(stock_symbol)
 
 
 @app.post("/query_10k")
 def read_item(query: str):
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
     response = rag_chain.invoke(query)
-    # print(response)
     return response
 
 
